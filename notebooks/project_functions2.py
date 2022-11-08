fb7764b3b58015ac2aef7ad0bbc22804d25c2e27
Raghav Bhagria <YOUR@EMAIL.com>
2022-11-07 22:25:12 -0800

    Milestone done

---
+++ notebooks/project_functions2.py
@@ -0,0 +1,44 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "3c229d5b-e03f-4d95-8f99-424e0734fd70",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Raghav's method chaining function"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "626ad3cd-0f0b-488e-b4f6-a0ff38c9814e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.13"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+    
+}
