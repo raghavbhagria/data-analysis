f468a0a73eb3f32818da8bd15ba92500c38bfe3a
Raghav Bhagria <YOUR@EMAIL.com>
2022-12-02 15:55:15 -0800

    Milestone 5 done

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -1,44 +1,17 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "3c229d5b-e03f-4d95-8f99-424e0734fd70",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Raghav's method chaining function"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "626ad3cd-0f0b-488e-b4f6-a0ff38c9814e",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.13"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
+
+
+import pandas as pd
+
+def file_Loading(path):
+   
+    # Method Chain 1 (Load data, drop na rows, drop unneeded columns)
     
-}
+    # Preparing list needed
+    drop_columns = ['registeration','Ground','Location','time']
+    def clean_file:
+        df1 = (pd.read_csv("/Users/raghavbhagria/Desktop/COSC301/project-group46/data/processed/processed.csv").dropna().drop(drop_columns, axis=1).reset_index(drop=True))
+        
+    def rename_col:
+        df2=df1.rename(columns={'Fatilities':'Deaths'}, inplace=True)
+    
+    return df2
